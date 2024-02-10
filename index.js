177654e1ddc17ff6f6d94db7e2ca229bbe2e8d33
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-02-10 20:34:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
+const conf = require('../set');
 
 // Remplacez 'YOUR_TOKEN' par le token de votre bot
 const token = '6467806947:AAGL74S28MeTHz9qNwjA1cnb-f8sgUewPnM';
@@ -6,46 +7,25 @@ const token = '6467806947:AAGL74S28MeTHz9qNwjA1cnb-f8sgUewPnM';
 // Créer un bot qui utilise le token d'authentification
 const bot = new TelegramBot(token, { polling: true });
 
-   function repondre(mes) { bot.sendMessage(chatId, mes);
-}
-function image(mes1,mes2) { bot.sendPhoto(chatId, mes1 , { caption: mes2 });
-  }
-// Commande '/start'
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Salut! Je suis votre bot Telegram.');
-});
-
-// Commande '/hello'
-bot.onText(/\/hello/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Bonjour! Comment allez-vous?');
-});
-
-// Commande '/info'
-bot.onText(/\/info/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Voici quelques informations sur moi...');
-});
-
 // Événement déclenché lorsque le bot reçoit un message texte
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const textReceived = msg.text;
+  const userId = msg.from.id;
+  const nomAuteurMessage = msg.pushName;
+
+function repondre(mes) { bot.sendMessage(chatId, mes);
+};
+function image(mes1,mes2) { bot.sendPhoto(chatId, mes1 , { caption: mes2 });
+  };
 
   // Répondre à un message specifique
-  if (textReceived.toLowerCase().includes('salut')) {
-    // Envoyer un message de salutation avec une image
-    bot.sendPhoto(chatIdl, 'https://telegra.ph/file/2d1eabc6e026cdb542395.jpg' , { caption: 'Salut! Voici une image.' });
-  }
-});
-               const arg = 
-                var idBot = decodeJid(zk.user.id);
-                var servBot = idBot.split('@')[0];
-                const nomAuteurMessage = msg.pushName;
-                const fatao = '5829888322' ;
-                const white = '6912879147' ;
-                const superUser = [servBot, fatao, white, Conf.SUDO_ID | "");
+  //if (textReceived.toLowerCase().includes('salut'))
+                const arg = textReceived.split(' ');
+                const Bot = '@NEOverse_2k24_bot';
+                const fatao = '5829888322';
+                const white = '6912879147';
+                const superUser = [Bot, fatao, white, Conf.SUDO_ID || "");
                 
                 console.log("[][]...{NEOverse-Md}...[][]");
                 console.log("=========== Nouveau message ===========");
@@ -62,8 +42,10 @@ function mybotpic() {
     }
 
 
-var commandeOptions = {
+const commandeOptions = {
                     superUser, dev, arg, 
-                   mybotpic, image
+                   mybotpic,image,bot,repondre
                 
                 };
+
+});
