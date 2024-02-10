bba262bf864b635f82134eed26601a8311f59a7a
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-02-10 21:20:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const token = '6467806947:AAGL74S28MeTHz9qNwjA1cnb-f8sgUewPnM';
 
 // Créer un bot qui utilise le token d'authentification
 const bot = new TelegramBot(token, { polling: true });
+let chatId, textReceived, userId, nomAuteurMessage;
 
 // Événement déclenché lorsque le bot reçoit un message texte
 bot.on('message', (msg) => {
@@ -25,7 +26,7 @@ function image(mes1,mes2) { bot.sendPhoto(chatId, mes1 , { caption: mes2 });
                 const Bot = '@NEOverse_2k24_bot';
                 const fatao = '5829888322';
                 const white = '6912879147';
-                const superUser = [Bot, fatao, white, Conf.SUDO_ID || "");
+                const superUser = [Bot, fatao, white, conf.SUDO_ID || "");
                 
                 console.log("[][]...{NEOverse-Md}...[][]");
                 console.log("=========== Nouveau message ===========");
@@ -33,7 +34,7 @@ function image(mes1,mes2) { bot.sendPhoto(chatId, mes1 , { caption: mes2 });
                 console.log("------ contenu du message ------");
                 console.log(textReceived);
 function mybotpic() {
-   const liens = 
+   const liens = ['https://telegra.ph/file/e4f27e467089eb3e31463.jpg' , 'https://telegra.ph/file/00fd279ccd45bef04b52a.jpg', 'https://telegra.ph/file/d8a070a1d819297ed8b29.jpg', 'https://telegra.ph/file/e9128988e705cc33ce72f.jpg'].split(',');     
       // Générer un indice aléatoire entre 0 (inclus) et la longueur du tableau (exclus)
       const indiceAleatoire = Math.floor(Math.random() * liens.length);
       // Récupérer le lien correspondant à l'indice aléatoire
