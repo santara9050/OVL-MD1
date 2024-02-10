45c6104649a31915c2b3d258a4c2f032ee5447b5
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-02-10 14:08:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // Remplacez 'YOUR_TOKEN' par le token de votre bot
-const token = 'YOUR_TOKEN';
+const token = '6467806947:AAGL74S28MeTHz9qNwjA1cnb-f8sgUewPnM';
 
 // Créer un bot qui utilise le token d'authentification
 const bot = new TelegramBot(token, { polling: true });
@@ -32,6 +32,6 @@ bot.on('message', (msg) => {
   // Répondre à un message spécifique
   if (textReceived.toLowerCase().includes('salut')) {
     // Envoyer un message de salutation avec une image
-    bot.sendPhoto(chatId, 'URL_DE_VOTRE_IMAGE', { caption: 'Salut! Voici une image.' });
+    bot.sendPhoto(chatId, 'https://telegra.ph/file/2d1eabc6e026cdb542395.jpg' , { caption: 'Salut! Voici une image.' });
   }
 });
