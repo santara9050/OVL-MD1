fdfcc8f84fee55b96c000fd174b458c4587103a3
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-02-11 17:56:24 +0000

    Create start.js

---
+++ commandes/start.js
@@ -0,0 +1,7 @@
+async (bot, commandeOptions) => {
+    let { repondre } = commandeOptions;
+bot.onText(/\/start/, (msg) => {
+        const chatId = msg.chat.id;
+        const nomAuteurMessage = msg.pushName;
+  let nss = `${nomAuteurMessage} salut je suis votre bot telegram`
+  repondre(nss);
