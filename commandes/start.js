d29ddb975979dd279507b8ddbcb525ed7e1e9c26
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-02-11 22:16:24 +0000

    Update start.js

--- commandes/start.js
+++ commandes/start.js
@@ -1,7 +1,7 @@
 const { bot/*, repondre*/ } = require('../fonctions');
  bot.onText(/\/start/, (msg) => {
         const chatId = msg.chat.id;
-        const nomAuteurMessage = msg.pushName;
-        let nss = `${nomAuteurMessage} salut je suis votre bot telegram`;
+        const nomAuteurMessage = msg.from.first_name;
+        let nss = `${nomAuteurMessage} salut je suis NEOverse-Md un bot telegram developpé par @Fatao_2580`;
         bot.sendMessage(chatId, nss);
     });
