d0b53d4e30622001030f4d9c483f82057186d25d
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-02-11 15:00:30 +0000

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -1,11 +1,11 @@
-const { bot, repondre, image, video, mybotpic } = require('../index');
 const conf = require('../set');
 const commands = {
     "Général": ["Info", "Menu", "Dev"],
     "NEOverse": ["North1", "North2", "North3", "East1", "East2", "East3", "West1", "West2", "West3", "Central1", "Central2", "Central3", "Fa1", "Fa2", "Dt1", "Dt2", "Zt1", "Zt2", "Nm1", "Nm2", "Uf1", "Uf2", "Nba1", "Nba2"]
 };
 
-const afficherMenu = () => {
+async (bot, commandeOptions) => {
+    let { repondre, image, video, nomAuteurMessage, mybotpic } = commandeOptions;
 bot.onText(new RegExp(`${conf.PREFIXE}menu`), (msg) => {
         const chatId = msg.chat.id;
         const nomAuteurMessage = msg.pushName;
