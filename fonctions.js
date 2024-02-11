69dd68469a88c21fc7706c3338879fcd99851939
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-02-11 21:15:44 +0000

    Update fonctions.js

--- fonctions.js
+++ fonctions.js
@@ -1,10 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
-const conf = require('./set');
-
-// Remplacez 'YOUR_BOT_TOKEN' par le token de votre bot
 const token = '6467806947:AAGL74S28MeTHz9qNwjA1cnb-f8sgUewPnM';
 const bot = new TelegramBot(token, { polling: true });
-const dev = ['5829888322', '6912879147', conf.SUDO_ID || ''];
+const dev = ['5829888322', '6912879147'];
 
 function repondre(chatId, message) {
     bot.sendMessage(chatId, message);
