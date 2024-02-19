6414b27c1c6c59f13d5584ee19bdabe8720933d5
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-02-19 19:02:23 +0000

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -1,4 +1,4 @@
-const { bot,/* repondre, image, video, nomAuteurMessage,mybotpic*/ lienAleatoire } = require('../fonctions');
+const { bot,/* repondre, image, video, nomAuteurMessage,mybotpic, lienAleatoire*/ } = require('../fonctions');
     
     //const conf = require('../set');
 const commands = { 
@@ -29,7 +29,7 @@ Voici la liste de mes commandes :\n`;
             infomsg += `╚════ ✵ ✵ ═══╝\n`;
         }
 
-        const lien = lienAleatoire;
+        const lien = mybotpic()
 
         if (lien.match(/\.(mp4|gif)$/i)) {
             try {
@@ -49,3 +49,15 @@ Voici la liste de mes commandes :\n`;
             }
         }
     });
+function mybotpic() {
+
+const liens = [
+        'https://telegra.ph/file/e4f27e467089eb3e31463.jpg', 
+        'https://telegra.ph/file/00fd279ccd45bef04b52a.jpg', 
+        'https://telegra.ph/file/d8a070a1d819297ed8b29.jpg', 
+        'https://telegra.ph/file/e9128988e705cc33ce72f.jpg'
+    ];
+    const indiceAleatoire = Math.floor(Math.random() * liens.length);
+    const lienAleatoire = liens[indiceAleatoire];
+    return lienAleatoire;
+}
