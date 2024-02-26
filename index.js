c43b1763fd00cf1b800810e7711f6390243a32aa
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-02-26 21:14:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
-const baileys = require("@whiskeysockets/baileys");
-const logger = require("@whiskeysockets/baileys/lib/Utils/logger").default.child({});
+const baileys_1 = require("@whiskeysockets/baileys");
+const logger_1 = require("@whiskeysockets/baileys/lib/Utils/logger");
+const logger = logger_1.default.child({});
 logger.level = 'silent';
-const boom = require("@hapi/boom");
-const conf = require("./set"); // Supposons que 'set.js' contient la configuration nécessaire
+const boom_1 = require("@hapi/boom");
+const conf = require("./set");
 const axios = require("axios");
 const pino = require("pino");
 const { toBuffer } = require("qrcode");
 const path = require('path');
 const fs = require("fs-extra");
-
 const session = conf.SESSION_ID || "";
 
-async function authentication() {
+async function authentification() {
     try {
         if (!fs.existsSync(__dirname + "/auth_info_baileys/creds.json")) {
-            console.log("Connexion en cours ...");
+            console.log("connexion en cours ...");
             await fs.writeFileSync(__dirname + "/auth_info_baileys/creds.json", atob(session), "utf8");
         } else if (fs.existsSync(__dirname + "/auth_info_baileys/creds.json") && session !== "ovl") {
             await fs.writeFileSync(__dirname + "/auth_info_baileys/creds.json", atob(session), "utf8");
         }
     } catch (e) {
-        console.log("Session invalide " + e);
+        console.log("Session Invalide " + e);
         return;
     }
 }
 
-authentication();
+authentification();
 
-const store = baileys.makeInMemoryStore({
+const store = baileys_1.makeInMemoryStore({
     logger: pino().child({ level: "silent", stream: "store" }),
 });
-
-async function main() {
-    try {
-        const { version, isLatest } = await baileys.fetchLatestBaileysVersion();
-        const { state, saveCreds } = await baileys.useMultiFileAuthState(__dirname + "/auth_info_baileys");
+setTimeout(async () => {
+    async function main() {
+        const { version, isLatest } = await baileys_1.fetchLatestBaileysVersion();
+        const { state, saveCreds } = await baileys_1.useMultiFileAuthState(__dirname + "/auth_info_baileys");
         const sockOptions = {
             version,
             logger: pino({ level: "silent" }),
@@ -49,7 +48,7 @@ async function main() {
             keepAliveIntervalMs: 30_000,
             auth_info_baileys: {
                 creds: state.creds,
-                keys: baileys.makeCacheableSignalKeyStore(state.keys, logger),
+                keys: baileys_1.makeCacheableSignalKeyStore(state.keys, logger),
             },
             getMessage: async (key) => {
                 if (store) {
@@ -57,27 +56,65 @@ async function main() {
                     return msg.message || undefined;
                 }
                 return {
-                    conversation: 'Une erreur s\'est produite. Veuillez répéter la commande.'
+                    conversation: 'An Error Occurred, Repeat Command!'
                 };
             }
         };
 
-        const ovl = baileys.default(sockOptions);
+        const ovl = baileys_1.default(sockOptions);
 
         ovl.ev.on("messages.upsert", async (m) => {
             const { messages } = m;
             const ms = messages[0];
             if (!ms.message) return;
+const decodeJid = (jid) => {
+                    if (!jid)
+                        return jid;
+                    if (/:\d+@/gi.test(jid)) {
+                        let decode = (0, baileys_1.jidDecode)(jid) || {};
+                        return decode.user && decode.server && decode.user + '@' + decode.server || jid;
+                    }
+                    else
+                        return jid;
+                };
+var mtype = (0, baileys_1.getContentType)(ms.message);
+                var texte = mtype == "conversation" ? ms.message.conversation : mtype == "imageMessage" ? ms.message.imageMessage?.caption : mtype == "videoMessage" ? ms.message.videoMessage?.caption : mtype == "extendedTextMessage" ? ms.message?.extendedTextMessage?.text : mtype == "buttonsResponseMessage" ?
+                    ms?.message?.buttonsResponseMessage?.selectedButtonId : mtype == "listResponseMessage" ?
+                    ms.message?.listResponseMessage?.singleSelectReply?.selectedRowId : mtype == "messageContextInfo" ?
+                    (ms?.message?.buttonsResponseMessage?.selectedButtonId || ms.message?.listResponseMessage?.singleSelectReply?.selectedRowId || ms.text) : "";
+                var origineMessage = ms.key.remoteJid;
+                var idBot = decodeJid(ovl.user.id);
+                var servBot = idBot.split('@')[0];
+                const verifGroupe = origineMessage?.endsWith("@g.us");
+                var infosGroupe = verifGroupe ? await ovl.groupMetadata(origineMessage) : "";
+                var nomGroupe = verifGroupe ? infosGroupe.subject : "";
+                var msgRepondu = ms.message.extendedTextMessage?.contextInfo?.quotedMessage;
+                var auteurMsgRepondu = decodeJid(ms.message?.extendedTextMessage?.contextInfo?.participant);
+                //ms.message.extendedTextMessage?.contextInfo?.mentionedJid
+                // ms.message.extendedTextMessage?.contextInfo?.quotedMessage.
+                var mr = ms.message?.extendedTextMessage?.contextInfo?.mentionedJid;
+                var utilisateur = mr ? mr : msgRepondu ? auteurMsgRepondu : "";
+                var auteurMessage = verifGroupe ? (ms.key.participant ? ms.key.participant : ms.participant) : origineMessage;
+                if (ms.key.fromMe) {
+                    auteurMessage = idBot;
+                }
 
-            // Logique de traitement des messages ici
-
-            console.log("=========== Nouveau message ===========");
-            console.log("Type de message : " + mtype);
-            console.log("Contenu du message : " + texte);
-            console.log("=======================================");
-        });
-        // Fonction pour charger les commandes depuis les fichiers externes
-async function loadCommands() {
+                var membreGroupe = verifGroupe ? ms.key.participant : '';
+                const nomAuteurMessage = ms.pushName;
+                const verifCom = texte ? texte.startsWith(prefixe) : false;
+                const com = verifCom ? texte.slice(1).trim().split(/ +/).shift().toLowerCase() : false;
+console.log("=========== Nouveau message ===========");
+                if (verifGroupe) {
+                    console.log("message provenant du groupe : " + nomGroupe);
+                }
+                console.log("message envoyé par : " + "[" + nomAuteurMessage + " : " + auteurMessage.split("@s.whatsapp.net")[0] + " ]");
+                console.log("type de message : " + mtype);
+                console.log("------ contenu du message ------");
+                console.log(texte);
+function repondre(mes) {
+                ovl.sendMessage(origineMessage, { text: mes }, { quoted: ms });
+            }
+            async function loadCommands() {
     console.log("Chargement des commandes...");
     const commandsDir = path.join(__dirname, 'commandes');
     try {
@@ -102,12 +139,8 @@ async function loadCommands() {
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-   loadCommands();     
-    } catch (error) {
-        console.error("Une erreur s'est produite :", error);
+   loadCommands();
+        });
     }
-}
-
-main();
-
-
+        main()
+});
