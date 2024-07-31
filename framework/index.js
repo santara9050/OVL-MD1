8d9eab55385c248a3272e61350702ced5945ed88
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-07-31 15:47:57 +0000

    Update index.js

--- framework/index.js
+++ framework/index.js
@@ -1,15 +1,13 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.genererNomFichier = exports.stick = exports.format = exports.styletext = exports.ovJson = exports.getBuffer = exports.reaction = exports.police = exports.ovlcmd = void 0;
+exports.genererNomFichier =/*exports.stick = exports.format = exports.styletext = exports.ovJson = exports.getBuffer = exports.reaction = exports.police =*/ exports.ovlcmd = void 0;
 let { ovlcmd } = require("./ovlcmd");
 exports.ovlcmd = ovlcmd;
 const mesfonctions_1 = require("./mesfonctions");
-Object.defineProperty(exports, "reaction", { enumerable: true, get: function () { return mesfonctions_1.reaction; } });
+/*Object.defineProperty(exports, "reaction", { enumerable: true, get: function () { return mesfonctions_1.reaction; } });
 Object.defineProperty(exports, "police", { enumerable: true, get: function () { return mesfonctions_1.police; } });
 Object.defineProperty(exports, "getBuffer", { enumerable: true, get: function () { return mesfonctions_1.getBuffer; } });
 Object.defineProperty(exports, "ovJson", { enumerable: true, get: function () { return mesfonctions_1.ovJson; } });
 Object.defineProperty(exports, "format", { enumerable: true, get: function () { return mesfonctions_1.format; } });
 Object.defineProperty(exports, "styletext", { enumerable: true, get: function () { return mesfonctions_1.styletext; } });
-Object.defineProperty(exports, "stick", { enumerable: true, get: function () { return mesfonctions_1.stick; } });
+Object.defineProperty(exports, "stick", { enumerable: true, get: function () { return mesfonctions_1.stick; } });*/
 Object.defineProperty(exports, "genererNomFichier", { enumerable: true, get: function () { return mesfonctions_1.genererNomFichier; } });
 var { reagir } = require("./app");
