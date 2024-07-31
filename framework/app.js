788c942737c7201940b4e580a348614d8725bc46
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-07-31 15:43:22 +0000

    Update app.js

--- framework/app.js
+++ framework/app.js
@@ -1,5 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 exports.reagir = void 0;
 async function reagir(dest, ovl, msg, emoji) {
     await ovl.sendMessage(dest, { react: { text: emoji, key: msg.key } });
