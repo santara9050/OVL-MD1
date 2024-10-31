6fd4113199b78e6630bed1ec034cb11f81b4939f
Ainz <140420451+Nignanfatao@users.noreply.github.com>
2024-10-31 17:02:30 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,53 +1,8 @@
-const fs = require('fs-extra');
-const { Sequelize } = require('sequelize');
-if (fs.existsSync('set.env'))
-    require('dotenv').config({ path: __dirname + '/set.env' });
-const path = require("path");
-const databasePath = path.join(__dirname, './database.db');
-const DATABASE_URL = process.env.DATABASE_URL === undefined
-    ? databasePath
-    : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || 'ovl',
-    // ETAT:process.env.ETAT,
-    PREFIXE: process.env.PREFIXE,
-    NOM_OWNER: process.env.NOM_OWNER || "Zokou-Md",
-    LIENS_MENU: process.env.LIENS_MENU || "",
-    SESSION_ID: process.env.SESSION_ID || "",
-   // NUMERO_OWNER : process.env.NUMERO_OWNER,              
-    //LECTURE_AUTO_STATUS: process.env.LECTURE_AUTO_STATUS || "non",
-   // TELECHARGER_AUTO_STATUS: process.env.TELECHARGER_AUTO_STATUS || 'non',
-   // MODE: process.env.MODE_PUBLIC,
-    //PM_PERMIT: process.env.PM_PERMIT || 'non',
-    BOT : process.env.NOM_BOT || 'OVL-Md',
-    //URL : process.env.LIENS_MENU || 'https://telegra.ph/file/0d81626ca4a81fe93303a.jpg',
-    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
-    HEROKU_API_KEY : process.env.HEROKU_APY_KEY ,
-    //WARN_COUNT : process.env.WARN_COUNT || '3' ,
-    //GPT : process.env.OPENAI_API_KEY,
-    //DP : process.env.STARTING_BOT_MESSAGE,              
-    DATABASE_URL,
-    DATABASE: DATABASE_URL === databasePath
-        ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9",
-    /* new Sequelize({
-     dialect: 'sqlite',
-     storage: DATABASE_URL,
-     logging: false,
-})
-: new Sequelize(DATABASE_URL, {
-     dialect: 'postgres',
-     ssl: true,
-     protocol: 'postgres',
-     dialectOptions: {
-         native: true,
-         ssl: { require: true, rejectUnauthorized: false },
-     },
-     logging: false,
-}),*/
+module.exports = {
+    PREFIXE: process.env.PREFIXE || ".",
+    NOM_OWNER: process.env.NOM_OWNER || "Ainz",
+    MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
+    SESSION_ID: process.env.SESSION_ID || "ovl",
+    NOM_BOT : process.env.NOM_BOT || 'OVL-MD',
+    DATABASE: process.env.DATABASE || 'postgresql://postgres.zacfshdorkmodrenhalb:famillenignan1@@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
 };
-let fichier = require.resolve(__filename);
-fs.watchFile(fichier, () => {
-    fs.unwatchFile(fichier);
-    console.log(`mise à jour ${__filename}`);
-    delete require.cache[fichier];
-    require(fichier);
-});
