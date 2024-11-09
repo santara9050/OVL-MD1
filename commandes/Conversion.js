d53e782bfde82a241cced7d6a2afe35bb719ba59
Ainz <140420451+Nignanfatao@users.noreply.github.com>
2024-11-09 18:19:20 +0000

    Create Conversion.js

---
+++ commandes/Conversion.js
@@ -0,0 +1,28 @@
+const { ovlcmd, cmd } = require("../framework/ovlcmd");
+const { upload } = require("telegra.ph");
+
+ovlcmd(
+    {
+        nom_cmd: "telegraph_upload",
+        classe: "upload",
+        react: "📤",
+        desc: "Upload une image ou vidéo sur Telegraph et renvoie le lien"
+    },
+    async (ms_org, ovl, cmd_options) => {
+      const msg_Repondu = cmd_options;
+        if (!msg_Repondu.imageMessage && !msg_Repondu.videoMessage) {
+            return ovl.sendMessage(ms_org, { text: "Veuillez mentionner une image ou une vidéo" });
+        }
+        const mediaMessage = msg_Repondu.imageMessage || msg_Repondu.videoMessage;
+        const mediaBuffer = await ovl.dl_save_media_ms(mediaMessage);
+
+        try {
+            const response = await upload(mediaBuffer, "media.jpg");
+            const link = response.url;
+            await ovl.sendMessage(ms_org, { text: link });
+        } catch (error) {
+            console.error("Erreur lors de l'upload sur Telegraph:", error);
+            await ovl.sendMessage(ms_org, { text: "Erreur lors de la création du lien Telegraph." });
+        }
+    }
+);
