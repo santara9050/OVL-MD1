0111864e0eda48987c80da6f042f43a940e63725
Ainz <140420451+Nignanfatao@users.noreply.github.com>
2024-11-10 18:23:31 +0000

    Update set.js

--- set.js
+++ set.js
@@ -5,6 +5,5 @@ module.exports = {
     MODE: process.env.MODE || "public",
     MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
     SESSION_ID: process.env.SESSION_ID || "ovl",
-    NOM_BOT : process.env.NOM_BOT || 'OVL-MD',
-    DATABASE: process.env.DATABASE || 'postgresql://postgres.zacfshdorkmodrenhalb:famillenignan1@@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
+    DATABASE: process.env.DATABASE || 'postgresql://postgres.qnjvgxwyncnsbpfxwrbq:ovlmdmdpasse@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
 };
