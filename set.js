d2185bf8425aee174c07e637539ddec571031eb9
Ainz <140420451+Nignanfatao@users.noreply.github.com>
2024-11-10 08:29:22 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,6 +1,8 @@
 module.exports = {
     PREFIXE: process.env.PREFIXE,
     NOM_OWNER: process.env.NOM_OWNER || "Ainz",
+    NUMERO_OWNER: process.env. NUMERO_OWNER,
+    MODE: process.env.MODE || "public",
     MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
     SESSION_ID: process.env.SESSION_ID || "ovl",
     NOM_BOT : process.env.NOM_BOT || 'OVL-MD',
