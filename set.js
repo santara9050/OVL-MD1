334d50ea31df5254535d5d95f7ea217a1ef3436b
Ainz <140420451+Nignanfatao@users.noreply.github.com>
2024-11-10 19:00:09 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
-    PREFIXE: process.env.PREFIXE,
-    NOM_OWNER: process.env.NOM_OWNER || "Ainz",
-    NUMERO_OWNER: process.env. NUMERO_OWNER,
-    MODE: process.env.MODE || "public",
-    MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
-    SESSION_ID: process.env.SESSION_ID || "ovl",
-    DATABASE: process.env.DATABASE || 'postgresql://postgres.qnjvgxwyncnsbpfxwrbq:ovlmdmdpasse@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
+  PREFIXE: process.env.PREFIXE || "",
+  NOM_OWNER: process.env.NOM_OWNER || "Ainz",
+  NUMERO_OWNER: process.env.NUMERO_OWNER || "",
+  MODE: process.env.MODE || "public",
+  MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
+  SESSION_ID: process.env.SESSION_ID || "ovl",
+  DATABASE: process.env.DATABASE || 'postgresql://postgres.qnjvgxwyncnsbpfxwrbq:ovlmdmdpasse@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
 };
