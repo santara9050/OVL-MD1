26a804d27f39e403602f15b12b4dc7400cff5d83
nignanfatao1 <nignanfatao170@gmail.com>
2024-12-08 11:39:21 +0000

    Create Fun.js

---
+++ commandes/Fun.js
@@ -0,0 +1,133 @@
+const { ovlcmd } = require("../framework/ovlcmd");
+const fancy = require("../framework/style");
+const fs = require('fs');
+const axios = require('axios');
+
+ovlcmd(
+    {
+        nom_cmd: "fliptext",
+        classe: "Fun",
+        desc: "Inverse le texte fourni.",
+    },
+    async (ms_org, ovl, cmd_options) => {
+        const { arg, ms } = cmd_options;
+        const text = arg.join(" ");
+        if (!text) {
+            return await ovl.sendMessage(ms_org, { text: "Veuillez fournir un texte à inverser !" });
+        }
+        const flipped = text.split("").reverse().join("");
+        await ovl.sendMessage(ms_org, { text: flipped }, { quoted: ms });
+    }
+);
+
+ovlcmd(
+    {
+        nom_cmd: "readmore",
+        classe: "Fun",
+        desc: "Ajoute un effet 'voir plus' au texte.",
+    },
+    async (ms_org, ovl, cmd_options) => {
+        const { arg, ms } = cmd_options;
+        const text = arg.join(" ");
+        if (!text) {
+            return await ovl.sendMessage(ms_org, { text: "Veuillez fournir un texte" });
+        }
+        const hiddenText = `${text.split(" ").join(" ")}${String.fromCharCode(8206).repeat(4001)}`;
+        await ovl.sendMessage(ms_org, { text: hiddenText }, { quoted: ms });
+    }
+);
+
+ovlcmd(
+    {
+        nom_cmd: "ship",
+        classe: "Fun",
+        desc: "Test de compatibilité entre deux personnes",
+        alias: ["match"],
+    },
+    async (ms_org, ovl, cmd_options) => {
+        const { auteur_Msg_Repondu, auteur_Message, arg, ms } = cmd_options;
+        const tags = auteur_Msg_Repondu || (arg[0]?.includes("@") && `${arg[0].replace("@", "")}@s.whatsapp.net`);
+         if (tags.length === 0) {
+            return await ovl.sendMessage(ms_org, { text: "Mentionne une personne" });
+         }
+        const randomPercentage = Math.floor(Math.random() * 101);
+        let comment;
+        if (randomPercentage <= 30) {
+            comment = "💔 Pas vraiment compatibles... 😢";
+        } else if (randomPercentage <= 70) {
+            comment = "🤔 Il y a du potentiel, mais cela demande du travail !";
+        } else {
+            comment = "💖 Vous êtes faits l'un pour l'autre ! 🌹";
+
+        }
+
+        await ovl.sendMessage(ms_org, {
+            text: `💘 *Ship*\n\n👩‍💻 *Madame* : @${tags[0].split("@")[0]}\n🧑‍💻 *Monsieur* : @${author.split("@")[0]}\n\n💖 Compatibilité : *${randomPercentage}%*\n\n📝 *Commentaire* : ${comment}`,
+            mentions: [tags[0], auteur_Message],
+        });
+    }
+);
+ 
+ovlcmd(
+    {
+        nom_cmd: "couplepp",
+        classe: "Fun",
+        desc: "Envoie des photos de couple animées.",
+        alias: ["cpp"],
+    },
+    async (ms_org, ovl) => {
+        try {
+            const { data } = await axios.get("https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json");
+            const randomPicture = data[Math.floor(Math.random() * data.length)];
+            await ovl.sendMessage(ms_org, {
+                image: { url: randomPicture.female },
+                caption: "❤️ *Pour Madame 💁🏻‍♀️*",
+            });
+            await ovl.sendMessage(ms_org, {
+                image: { url: randomPicture.male },
+                caption: "❤️ *Pour Monsieur 💁🏻‍♂️*",
+            });
+        } catch (error) {
+            console.error("Erreur lors de la récupération des données :", error);
+            await ovl.sendMessage(ms_org, {
+                text: "❗ Impossible de récupérer les images. Réessaie plus tard.",
+            });
+        }
+    }
+);
+
+ovlcmd(
+  {
+    nom_cmd: "fancy",
+    classe: "Fun",
+    react: "✍️",
+    desc: "Applique un style fancy au texte",
+  },
+  async (ms_org, ovl, cmd_options) => {
+    const { arg, repondre } = cmd_options;
+    const id = parseInt(arg[0], 10);
+    const text = arg.slice(1).join(" ");
+    const prefixe = config.PREFIXE;
+
+    if (isNaN(id) || !text) {
+      return await repondre(
+        `\nExemple : ${prefixe}fancy 10 OVL-MD\n` +
+          String.fromCharCode(8206).repeat(4001) +
+          fancy.list("ovl-md", fancy)
+      );
+    }
+
+    try {
+      const selectedStyle = fancy[id - 1];
+      if (selectedStyle) {
+        return await repondre(fancy.apply(selectedStyle, text));
+      } else {
+        return await repondre(`_Style introuvable pour l'ID : ${id}_`);
+      }
+    } catch (error) {
+      console.error("Erreur lors de l'application du style :", error);
+      return await repondre("_Une erreur s'est produite :(_");
+    }
+  }
+);
+
