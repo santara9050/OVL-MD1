2a84012716948c52fca9532a3e7f7895671a2123
nignanfatao1 <nignanfatao170@gmail.com>
2024-12-11 08:31:12 +0000

    Update rank.js

--- DataBase/rank.js
+++ DataBase/rank.js
@@ -1,3 +1,18 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const config = require('../set');
+const db = config.DATABASE;
+
+const sequelize = new Sequelize(db, {
+  dialect: 'postgres',
+  ssl: true,
+  protocol: 'postgres',
+  dialectOptions: {
+    native: true,
+    ssl: { require: true, rejectUnauthorized: false },
+  },
+  logging: false,
+});
+
 const Ranks = sequelize.define('Ranks', {
     id: {
         type: DataTypes.STRING,
