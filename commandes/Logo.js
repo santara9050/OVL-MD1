f7b8aa42e83314865071b3da6df7128c1ed7e1b4
nignanfatao1 <nignanfatao170@gmail.com>
2024-12-14 19:27:04 +0000

    Update Logo.js

--- commandes/Logo.js
+++ commandes/Logo.js
@@ -93,7 +93,7 @@ const axios = require('axios');
 
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
 
-            "Origin": textpro.me,
+            "Origin": "textpro.me",
 
             "Referer": url,
 
