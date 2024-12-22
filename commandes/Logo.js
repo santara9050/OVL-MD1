1bcf6c9467bce485d580700db3e8a726feec79a8
nignanfatao1 <nignanfatao170@gmail.com>
2024-12-22 17:49:27 +0000

    Update Logo.js

--- commandes/Logo.js
+++ commandes/Logo.js
@@ -78,350 +78,411 @@ function addTextproCommand(nom_cmd, text_pro_url, desc, type) {
 addTextproCommand(
     "dragonball", // Nom de la commande
     "https://en.ephoto360.com/create-dragon-ball-style-text-effects-online-809.html", // URL du style
-    "Créer un logo Dragon Ball", // Description de la commande
     1 // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "deadpool", // Nom de la commande
     "https://en.ephoto360.com/create-text-effects-in-the-style-of-the-deadpool-logo-818.html", // URL du style
-    "Créez des effets de texte dans le style du logo Deadpool", // Description de la commande
     2 // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "blackpink", // Nom de la commande
     "https://en.ephoto360.com/create-a-blackpink-style-logo-with-members-signatures-810.html", // URL du style
-    "Créez un logo de style BLACKPINK avec les signatures des membres", // Description de la commande
     1 // Type : cette commande accepte un seul mot ou texte
 );
 
  addTextproCommand(
     "neon_bleu", // Nom de la commande
     "https://en.ephoto360.com/blue-neon-text-effect-117.html", // URL du style
-    "Effet de texte néon bleu", // Description de la commande
     1  // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "football", // Nom de la commande
     "https://en.ephoto360.com/paul-scholes-shirt-foot-ball-335.html", // URL du style
-    "Maillot de foot de Paul Scholes", // Description de la commande
     2 // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "acier_h", // Nom de la commande
     "https://en.ephoto360.com/heated-steel-lettering-effect-65.html", // URL du style
-    "Effet lettrage en acier chauffé", // Description de la commande
-    2 // Type : cette commande accepte un seul mot ou texte
+     2 // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "peint", // Nom de la commande
     "https://en.ephoto360.com/paint-splatter-text-effect-72.html", // URL du style
-    "Effet de texte avec éclaboussures de peinture", // Description de la commande
     1 // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
+    "thunder", // Nom de la commande
+    "https://en.ephoto360.com/thunder-text-effect-online-97.html", // URL du style
+    1 // Type : cette commande accepte un seul mot ou texte
+);
+
+addTextproCommand(
+    "thor", // Nom de la commande
+    "https://en.ephoto360.com/create-thor-logo-style-text-effects-online-for-free-796.html", // URL du style
+     1 // Type : cette commande accepte un seul mot ou texte
+);
+
+addTextproCommand(
+    "graffiti1", // Nom de la commande
+    "https://en.ephoto360.com/cute-girl-painting-graffiti-text-effect-667.html", // URL du style
+    2 // Type : cette commande accepte un seul mot ou texte
+);
+
+addTextproCommand(
+    "gold2", // Nom de la commande
+    "https://en.ephoto360.com/modern-gold-5-215.html", // URL du style
+     1 // Type : cette commande accepte un seul mot ou texte
+);
+
+addTextproCommand(
+    "neon_green", // Nom de la commande
+    "https://en.ephoto360.com/create-light-effects-green-neon-online-429.html", // URL du style
+     1 // Type : cette commande accepte un seul mot ou texte
+);
+
+addTextproCommand(
+    "effacer", // Nom de la commande
+    "https://en.ephoto360.com/create-eraser-deleting-text-effect-online-717.html", // URL du style
+     1 // Type : cette commande accepte un seul mot ou texte
+);
+
+addTextproCommand(
+    "galaxy", // Nom de la commande
+    "https://en.ephoto360.com/text-light-galaxy-effectt-345.html", // URL du style
+     1 // Type : cette commande accepte un seul mot ou texte
+);
+
+addTextproCommand(
+    "vintage", // Nom de la commande
+    "https://en.ephoto360.com/write-text-on-vintage-television-online-670.html", // URL du style
+    1 // Type : cette commande accepte un seul mot ou texte
+);
+
+addTextproCommand(
+    "gold1", // Nom de la commande
+    "https://en.ephoto360.com/gold-text-effect-158.html", // URL du style
+     1 // Type : cette commande accepte un seul mot ou texte
+);
+
+addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
+     // Type : cette commande accepte un seul mot ou texte
+);
+
+
+addTextproCommand(
+    "", // Nom de la commande
+    "", // URL du style
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
+     // Type : cette commande accepte un seul mot ou texte
+);
+
+
+addTextproCommand(
+    "", // Nom de la commande
+    "", // URL du style
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
+     // Type : cette commande accepte un seul mot ou texte
+);
+
+
+addTextproCommand(
+    "", // Nom de la commande
+    "", // URL du style
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
 addTextproCommand(
     "", // Nom de la commande
     "", // URL du style
-    "", // Description de la commande
      // Type : cette commande accepte un seul mot ou texte
 );
 
+addTextproCommand(
+    "", // Nom de la commande
+    "", // URL du style
+     // Type : cette commande accepte un seul mot ou texte
+);
+
+addTextproCommand(
+    "", // Nom de la commande
+    "", // URL du style
+     // Type : cette commande accepte un seul mot ou texte
+);
+
+
+addTextproCommand(
+    "", // Nom de la commande
+    "", // URL du style
+     // Type : cette commande accepte un seul mot ou texte
+);
+
+addTextproCommand(
+    "", // Nom de la commande
+    "", // URL du style
+     // Type : cette commande accepte un seul mot ou texte
+);
+
+addTextproCommand(
+    "", // Nom de la commande
+    "", // URL du style
+     // Type : cette commande accepte un seul mot ou texte
+);
+
+addTextproCommand(
+    "", // Nom de la commande
+    "", // URL du style
+     // Type : cette commande accepte un seul mot ou texte
+);
