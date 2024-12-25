556d3e735def5be6bf122d34974a490bbcb6f95c
nignanfatao1 <nignanfatao170@gmail.com>
2024-12-25 11:05:21 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config();
 module.exports = {
   PREFIXE: process.env.PREFIXE || "",
   NOM_OWNER: process.env.NOM_OWNER || "Ainz",
@@ -5,8 +7,8 @@ module.exports = {
   MODE: process.env.MODE || "public",
   MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
   SESSION_ID: process.env.SESSION_ID || "ovl",
-  LEVEL_UP: process.env.LEVEL_UP || 'non',
-  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || 'Wa-sticker',
-  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || 'OVL-MD',
-  DATABASE: process.env.DATABASE || 'postgresql://postgres.qnjvgxwyncnsbpfxwrbq:ovlmdmdpasse@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
+  LEVEL_UP: process.env.LEVEL_UP || "non",
+  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-sticker",
+  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "OVL-MD",
+  DATABASE: process.env.DATABASE || "postgresql://postgres.qnjvgxwyncnsbpfxwrbq:ovlmdmdpasse@aws-0-eu-central-1.pooler.supabase.com:6543/postgres",
 };
