3812646de2ee82598e568f274d79222f783d3088
Ainz-fk <ainzgit@gmail.com>
2025-03-25 11:14:11 +0000

    Create Antitag.js

---
+++ DataBase/Antitag.js
@@ -0,0 +1,60 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const config = require('../set');
+const db = config.DATABASE;
+
+const sequelize = new Sequelize(db, {
+  dialect: 'postgres',
+					ssl: true,
+					protocol: 'postgres',
+					dialectOptions: {
+						native: true,
+						ssl: { require: true, rejectUnauthorized: false },
+					},
+					logging: false,
+});
+
+const Antitag = sequelize.define('Antitag', {
+  id: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+  },
+  mode: {
+    type: DataTypes.STRING,
+    defaultValue: 'non',
+  },
+  type: {
+    type: DataTypes.ENUM('supp', 'warn', 'kick'),
+    defaultValue: 'supp',
+  },
+}, {
+  tableName: 'antitag',
+  timestamps: false,
+});
+
+const Antitag_warnings = sequelize.define('Antitag_warnings', {
+  groupId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  count: {
+    type: DataTypes.INTEGER,
+    defaultValue: 1,
+  },
+}, {
+  tableName: 'antitag_warnings',
+  timestamps: false,
+});
+
+(async () => {
+  await Antilink.sync();
+  console.log("Table 'Antitag' synchronisée avec succès.");
+
+  await Antitag_warnings.sync();
+  console.log("Table 'Antitag_warnings' synchronisée avec succès.");
+})();
+
+module.exports = { Antitag, Antitag_warnings };
