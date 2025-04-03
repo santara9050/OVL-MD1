fa00515c0152baa7cdfdfb1424f84230dc165c8e
Ainz-fkk <ainzgit@outlook.fr>
2025-04-03 05:15:34 +0000

    Update Settings.js

--- commandes/Settings.js
+++ commandes/Settings.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const { ovlcmd } = require("../framework/ovlcmd");
 const config = require('../set');
 const os = require('os');
-
+const { execSync } = require("child_process");
 const RENDER_API_KEY = config.RENDER_API_KEY;
 const host = os.hostname();
 const SERVICE_ID = host.split("-hibernate")[0]; 
@@ -115,41 +115,19 @@ async function getRenderCommit() {
   }
 }
 
-async function getGitCommit() {
+function getLastCommit() {
   try {
-    const response = await axios.get(
-      `https://api.github.com/repos/Ainz-fkk/OVL-MD/commits`,
-      {
-        headers: {
-          "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-          "Accept-Encoding": "gzip, deflate, br",
-          "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
-          "Sec-Ch-Ua": `"Not A(Brand";v="8", "Chromium";v="132"`,
-          "Sec-Ch-Ua-Mobile": "?1",
-          "Sec-Ch-Ua-Platform": `"Android"`,
-          "Sec-Fetch-Dest": "document",
-          "Sec-Fetch-Mode": "navigate",
-          "Sec-Fetch-Site": "none",
-          "Upgrade-Insecure-Requests": "1",
-          "User-Agent": "GoogleBot"
-        }
-      }
-    );
-
-    if (!response.data || response.data.length === 0) {
-      throw new Error("Aucun commit trouvé pour ce dépôt.");
-    }
-
-    const lastCommit = response.data[0];
-    return lastCommit.sha;
+    const lastCommit = execSync("git log -1 --pretty=format:'%h'", {
+      cwd: ".",
+      encoding: "utf-8",
+    }).trim();
+    
+    return lastCommit;
   } catch (error) {
-    console.error(error.message);
-    throw new Error("Impossible de récupérer le dernier commit depuis GitHub.");
+    console.error("Erreur lors de la récupération du commit:", error.message);
+    return null;
   }
 }
-
-// Test
-getGitCommit().then(console.log).catch(console.error);
 
 async function deployRender() {
   try {
