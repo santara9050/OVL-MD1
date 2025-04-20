06ae273e3b92ec31317682569b450f640c3f922d
santara9050 <mamadousantara48@gmail.com>
2025-04-20 15:23:53 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,15 +1,15 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  PREFIXE: process.env.PREFIXE || "",
-  NOM_OWNER: process.env.NOM_OWNER || "Ainz",
-  NUMERO_OWNER: process.env.NUMERO_OWNER || "",
+  PREFIXE: process.env.PREFIXE || "/",
+  NOM_OWNER: process.env.NOM_OWNER || "𓆩𝘗𝘌𝘙𝘍𝘌𝘊𝘛𝘐𝘖𝘕𓆪",
+  NUMERO_OWNER: process.env.NUMERO_OWNER || "22390500302",
   MODE: process.env.MODE || "public",
   MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
-  SESSION_ID: process.env.SESSION_ID || "ovl",
+  SESSION_ID: process.env.SESSION_ID || "Ovl-MD_DdhGYkMk_SESSION_ID",
   LEVEL_UP: process.env.LEVEL_UP || "non",
-  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-sticker",
-  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "OVL-MD",
+  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-𓆩𝘗𝘌𝘙𝘍𝘌𝘊𝘛𝘐𝘖𝘕𓆪",
+  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "｟ 𓆩𝘗𝘌𝘙𝘍𝘌𝘊𝘛𝘐𝘖𝘕 𝘒𝘈𝘙𝘈𓆪 ｠",
   DATABASE: process.env.DATABASE,
   RENDER_API_KEY: process.env.RENDER_API_KEY,
 };
