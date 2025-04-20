937709d2be84eee787499f9ff3b9677ba5399e29
santara9050 <mamadousantara48@gmail.com>
2025-04-20 17:32:23 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,15 +1,15 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  PREFIXE: process.env.PREFIXE || "/",
-  NOM_OWNER: process.env.NOM_OWNER || "𓆩𝘗𝘌𝘙𝘍𝘌𝘊𝘛𝘐𝘖𝘕𓆪",
-  NUMERO_OWNER: process.env.NUMERO_OWNER || "22390500302",
-  MODE: process.env.MODE || "public",
-  MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
-  SESSION_ID: process.env.SESSION_ID || "Ovl-MD_DdhGYkMk_SESSION_ID",
-  LEVEL_UP: process.env.LEVEL_UP || "non",
-  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-𓆩𝘗𝘌𝘙𝘍𝘌𝘊𝘛𝘐𝘖𝘕𓆪",
-  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "｟ 𓆩𝘗𝘌𝘙𝘍𝘌𝘊𝘛𝘐𝘖𝘕 𝘒𝘈𝘙𝘈𓆪 ｠",
-  DATABASE: process.env.DATABASE,
-  RENDER_API_KEY: process.env.RENDER_API_KEY,
+  PREFIXE=/
+NOM_OWNER=｟ 𓆩𝘗𝘌𝘙𝘍𝘌𝘊𝘛𝘐𝘖𝘕 𝘒𝘈𝘙𝘈𓆪 ｠
+NUMERO_OWNER=22390500302
+MODE=public
+MENU=https://i.ibb.co/ynx9QcZ/image.jpg
+SESSION_ID=Ovl-MD_r1vf3Jiq_SESSION-ID
+DATABASE=https://i.ibb.co/ynx9QcZ/image.jpg
+LEVEL_UP=non
+STICKER_PACK_NAME=Wa-Perfect
+STICKER_AUTHOR_NAME=𓆩𝘗𝘌𝘙𝘍𝘌𝘊𝘛𝘐𝘖𝘕𓆪
+RENDER_API_KEY=rnd_RkTvnzur2Z1y2aGicVtvHl15ksoY
 };
